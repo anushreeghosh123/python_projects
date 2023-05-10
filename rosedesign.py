5514867b70f4f522f445dc827cdce2d1fa21ded2
anushreeghosh123 <114284806+anushreeghosh123@users.noreply.github.com>
2023-05-10 23:22:49 +0530

    Add files via upload

---
+++ rosedesign.py
@@ -0,0 +1,38 @@
+from turtle import *
+import turtle as tur
+
+
+tur.penup ()
+tur.left (90)
+tur.fd (200)
+tur.pendown ()
+tur.right (90)
+
+
+tur.fillcolor ("red")
+tur.begin_fill ()
+tur.circle (10,180)
+tur.circle (25,110)
+tur.left (50)
+tur.circle (60,45)
+tur.circle (20,170)
+tur.right (24)
+tur.fd (30)
+tur.left (10)
+tur.circle (30,110)
+tur.fd (20)
+tur.left (40)
+tur.circle (90,70)
+tur.circle (30,150)
+tur.right (30)
+tur.fd (15)
+tur.circle (80,90)
+tur.left (15)
+tur.fd (45)
+tur.right (165)
+tur.fd (20)
+tur.left (155)
+tur.circle (150,80)
+tur.left (50)
+tur.circle (150,90)
+tur.end_fill()
